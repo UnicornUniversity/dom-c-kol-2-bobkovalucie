d94321afe4989c9102b11f6dd7e9cbed670acc6b
bobkovalucie <bobkova.luccka@gmail.com>
2025-11-09 17:39:02 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
+function convert (inputNumber, inputNumberSystem, outputNumberSystem){
+    let decNumber=0;
+    let arrDigits=inputNumber.split('').map(Number);       //create an array of separated digits (1 or 0)
+
+    for (let i = 0; i < arrDigits.length; i++){
+    let exponent = arrDigits.length-(i+1);
+    decNumber += arrDigits[i]*2**exponent;
+    }
+    return decNumber;
+}
+
 //TODO add/change doc as needed
 /**
  * TODO - Write functional code for this application. You can call any other function, but usage of ".toString(numberSystem)" and "Number.parseInt(number, numberSystem)" is forbidden (only permitted when used on individual digits).
@@ -9,17 +20,10 @@
  * @returns {string} containing number converted to output system
  */
 export function main(inputNumber, inputNumberSystem, outputNumberSystem) {
-	let decNumber=0;
-	let arrDigits=inputNumber.split('').map(Number);       //create an array of separated digits (1 or 0)
-
-    for (let i = 0; i < arrDigits.length; i++){
-		let exponent = arrDigits.length-(i+1);
-		decNumber += arrDigits[i]*2**exponent;
-    }
-  return decNumber;
+	let dtoOut = convert(1011, 2, 10);
+	console.log(dtoOut);
 }
 
-console.log(main("1011", 2,10)
 
 /**
  * TODO - Change this to contain all input number systems that your application can convert from.
